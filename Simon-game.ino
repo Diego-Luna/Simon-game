ad2e793f7db9470e1cd502851537b6de847052fa
Diego-Luna <diegofcolunalopez@gmail.com>
2022-03-26 08:52:22 -0600

    start proyect

---
+++ Simon-game.ino
@@ -0,0 +1,24 @@
+// > MoonMakers
+// -> Buttons
+int button_red = 7;
+int button_blue = 6;
+int button_green = 5;
+int button_yellow = 4;
+
+int buzzer = 12;
+
+int array_bottons[255] = {0};
+byte button_click = 0;
+
+void setup() {
+  pinMode(button_red, INPUT);
+  pinMode(button_blue, INPUT);
+  pinMode(button_green, INPUT);
+  pinMode(button_yellow, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
